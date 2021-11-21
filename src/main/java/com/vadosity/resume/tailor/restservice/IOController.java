a3cab174c74bd44265a8e3236e931af474e8beed
Brett <bshelley5732@menard-inc.com>
2021-11-20 22:46:01 -0500

    upgraded to use config service

--- src/main/java/com/vadosity/resume/tailor/restservice/IOController.java
+++ src/main/java/com/vadosity/resume/tailor/restservice/IOController.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -26,39 +27,33 @@ import com.vadosity.resume.tailor.DocxUpdate;
 import com.vadosity.resume.tailor.DocxUpdateRequest;
 import com.vadosity.resume.tailor.KeywordMatchResults;
 
-
-
-
 @RestController
 @RequestMapping("/resume")
 public class IOController {
 	
-	private static final String INPUT_DOCUMENT_SUFFIX = "_input.docx";
-	public static final String RESUME_OUTPUT_DIRECTORY;
 	private ObjectMapper objectMapper= new ObjectMapper();
 	
-	static {
-		
-		String dirPath = System.getProperty("user.home")
-				+ (System.getProperty("user.home").endsWith(File.separator)?"":File.separator)
-				+ "resume_tailor_docs";		
-		
-		try {
-			File f = new File(dirPath);
-			if ( !f.exists()) 
-			{
-				f.mkdirs();
-			}
-		}
-		catch ( Exception e) {
-			e.printStackTrace();
-		}
-		RESUME_OUTPUT_DIRECTORY = dirPath+File.separator;
+	@Value("${outputDocumentSuffix}")
+	private String outputDocumentSuffix;
+	
+	@Value("${inputDocumentSuffix}")
+	private String inputDocumentSuffix;
+	
+	@Value("${resumeOutputDirectory}")
+	private String resumeOutputDirectory;	
+	
+	@RequestMapping(path="/test", method=RequestMethod.GET)
+	public ResponseEntity<String> testMessage()
+	{
+		return ResponseEntity.ok(resumeOutputDirectory);
 	}
 	
 	@RequestMapping(path = "/download", method = RequestMethod.GET)
     public ResponseEntity<Resource> download(@RequestParam("filename") String filename) throws IOException {
-        File file = new File(RESUME_OUTPUT_DIRECTORY + filename);
+		verifyResumeOutputDirectory();
+		
+		
+        File file = new File(resumeOutputDirectory + filename);
 
         HttpHeaders header = new HttpHeaders();
         header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+filename);
@@ -80,6 +75,7 @@ public class IOController {
 	@PostMapping("/upload")
 	public ResponseEntity<KeywordMatchResults> upload(@RequestPart("file") MultipartFile multiPartFile,
 			@RequestParam(value = "json", defaultValue = "{}") String json) {
+		verifyResumeOutputDirectory();
 		
 		if (null == multiPartFile.getOriginalFilename()) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -109,8 +105,8 @@ public class IOController {
 	}
 	
 	private String determineUpdatedDocumentPath(String originalDocumentPath) {
-		String updatedPath = originalDocumentPath.substring(0,originalDocumentPath.lastIndexOf(INPUT_DOCUMENT_SUFFIX));
-		updatedPath+="_output.docx";
+		String updatedPath = originalDocumentPath.substring(0,originalDocumentPath.lastIndexOf(inputDocumentSuffix));
+		updatedPath+=outputDocumentSuffix;
 		return updatedPath;
 	}
 	
@@ -125,12 +121,22 @@ public class IOController {
 		
 		String origName=multiPartFile.getOriginalFilename();
 		String outputFilename=origName.substring(0,origName.lastIndexOf(".docx"));
-		outputFilename+=INPUT_DOCUMENT_SUFFIX;
-		String outputFilepath=RESUME_OUTPUT_DIRECTORY+outputFilename;
+		outputFilename+=inputDocumentSuffix;
+		String outputFilepath=resumeOutputDirectory+outputFilename;
 		Path path = Paths.get(outputFilepath);
 		Files.write(path, bytes,StandardOpenOption.CREATE,
 		         StandardOpenOption.TRUNCATE_EXISTING);
 		return outputFilepath;
+	}
+	
+	private void verifyResumeOutputDirectory() {
+		String dir = resumeOutputDirectory;
+		while ( dir.endsWith(File.separator)) {
+			dir=dir.substring(0,dir.lastIndexOf(File.separator));
+		}
+		File f = new File(dir);
+		if ( !f.exists()) f.mkdirs();
+		
 	}
 	
 }
--- src/main/java/com/vadosity/resume/tailor/restservice/RestServiceApplication.java
+++ src/main/java/com/vadosity/resume/tailor/restservice/RestServiceApplication.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
+@EnableAutoConfiguration
 @SpringBootApplication
 public class RestServiceApplication {
 
